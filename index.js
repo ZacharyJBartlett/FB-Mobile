3f3439adc5f9ab361ba84487603cc7080b7dff6f
ZacharyJB <Zachary.jamesbartlett@gmail.com>
2017-08-26 23:44:54 -0400

    created landing page/initializing firebase

---
+++ index.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+
+// initializing firebase
+const config = {
+    apiKey: "AIzaSyBYdQGojMAMm5Q6Vu6d9FPN-JVhv9XM8PQ",
+    authDomain: "helloworld-d9548.firebaseapp.com",
+    databaseURL: "https://helloworld-d9548.firebaseio.com",
+    projectId: "helloworld-d9548",
+    storageBucket: "",
+    messagingSenderId: "484299108181"
+  };
+
+  
